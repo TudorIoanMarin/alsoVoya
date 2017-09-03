822ae9e14cac2e676edd1280b4275a21774fce7b
Tudor Ioan Marin <tudor_marin10@yahoo.com>
2017-09-03 19:05:10 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, forwardRef } from '@angular/core';
+import { UpgradeAdapter } from '@angular/upgrade';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const upgradeAdapter: UpgradeAdapter = new UpgradeAdapter( forwardRef( () =>  AppModule ) );
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: '404', component: NotFoundComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    ContentComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot( routes )
   ],
   providers: [],
   bootstrap: [AppComponent]
